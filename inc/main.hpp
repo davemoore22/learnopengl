fe9d85bd7ba88d2d6833b779e4791808af4d6dc7
Dave M <davemoore22@gmail.com>
2022-05-30 21:15:20 +0100

    Initial Commit

---
+++ inc/main.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+// clang-format off
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+// clang-format onj
+
+#include <iostream>
+
+// https://learnopengl.com/Getting-started/Hello-Window
